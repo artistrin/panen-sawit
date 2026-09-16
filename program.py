5fdc098cd21979acb952e3635eaed32277ea676c
Khairina Putri <1247kkhairina@apps.ipb.ac.id>
2026-09-16 16:21:46 +0000

    Perubahan program oleh Anggota A

--- program.py
+++ program.py
@@ -1,7 +1,6 @@
-
 nama = input("Masukkan nama: ")
 luas = float(input("Masukkan luas lahan (ha): "))
 
-print("\nData Panen Sawit")
+print("\nData Panen Sawit Kelompok A")
 print("Nama:", nama)
 print("Luas lahan:", luas, "ha")
